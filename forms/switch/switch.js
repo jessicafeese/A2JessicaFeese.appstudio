eaed498f28bccd7c8840bc6e93845b81551eda27
jessicafeese <jaf32246@creighton.edu>
2021-09-19 14:59:25 -0500

    ifElse and switch

--- forms/switch/switch.js
+++ forms/switch/switch.js
@@ -1,5 +1,3 @@
-// Define variables
-
 let firstName = prompt('What is your first name?')
 console.log(firstName)
 
@@ -14,15 +12,21 @@ console.log(temp)
 let messages = [`${firstName}, wear a warm coat, hat, scarf and gloves.`,`${firstName}, wear a warm coat but you won't need a hat, scarf or gloves.`,`${firstName}, wear your warmest coat, a warm hat, a scarf, and warm gloves.`,`${firstName}, wear a warm coat, hat and gloves. Maybe a scarf too.`]
 console.log(messages)
 
-//Conditional Statements that takes into account the user's responses and gives them a certain response that is stored in the messages array
+//Using a switch keyword that takes into account the user's responses and gives them a certain response that is stored in the messages array
 
-if (temp < 32 && state == 'NE')
-  console.log(messages[0])
-else if (temp > 32 && temp < 50 && state == 'NE')
-  console.log(messages[1])
-else if (temp > 32 && temp < 50 && state == 'FL')
-  console.log(messages[2])
-else if (temp > 50 && temp < 70 && state == 'FL')
-  console.log(messages[3])
-else
-  console.log('Don\'t currently have a recommendation.')
+switch (true) {
+  case (temp < 32 && state == 'NE'):
+    console.log(messages[0])
+    break;
+  case (temp > 32 && temp < 50 && state == 'NE'):
+    console.log(messages[1])
+    break;
+  case (temp > 32 && temp < 50 && state == 'FL'):
+    console.log(messages[2])
+    break;
+  case (temp > 50 && temp < 70 && state == 'FL'):
+    console.log(messages[3])
+    break;
+  default: console.log('Don\'t currently have a recommendation.')
+    break;
+}
