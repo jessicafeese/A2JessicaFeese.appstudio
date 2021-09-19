02a5012647297f748131b686657e15adbce69fff
jessicafeese <jaf32246@creighton.edu>
2021-09-19 14:14:58 -0500

    prompts

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -1,9 +1,14 @@
 
 let firstName = prompt('What is your first name?')
 console.log(firstName)
+
 let state = prompt('What state do you live in? (NE,CO,etc.)')
 console.log(state)
+
 let temp = prompt('What is the current tempature outside? (degrees fahrenheit)')
 console.log(temp)
+
 let messages = [`${firstName},${state},${temp}`]
 console.log(messages)
+
+if (messages == [']
