86b12940a9c9ae009e12e907b8bc764e4f13e266
jessicafeese <jaf32246@creighton.edu>
2021-09-19 14:07:59 -0500

    Initial commit

---
+++ forms/switch/switch.js
@@ -0,0 +1,8 @@
+
+let firstName = prompt('What is your first name?')
+console.log(firstName)
+let state = prompt('What state do you live in? (NE,CO,etc.)')
+console.log(state)
+let temp = prompt('What is the current tempature outside? (degrees fahrenheit)')
+console.log(temp)
+let messages = [`${firstName},${state},${temp}`]
